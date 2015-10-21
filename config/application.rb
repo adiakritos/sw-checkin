d0bd3610147db8401ef6f3ef1a9f47359241faa8
Nick <adiakritos@gmail.com>
2015-10-20 17:41:48 -0700

    remove dotting from application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
 module SouthwestCheckin
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
